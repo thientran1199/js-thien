b31b2c51f04d957bf986b2ce45460f326a39de6b
thientran1199 <thientran1111999@gmail.com>
2019-10-03 10:55:30 +0700

    EX

---
+++ ex3.js
@@ -0,0 +1,12 @@
+class SinhVien{
+    // constructor(){
+    //     this.name = 'Nguyen Van Teo';
+    // }
+    name = 'ss3';
+    printSV = () =>{
+    console.log(this.name);
+    }
+
+}
+const sinhVien = new SinhVien();
+sinhVien.printSV();
